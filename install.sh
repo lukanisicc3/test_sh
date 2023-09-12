b7ed9b231e018fd14235a58b44423ad9e07e48d3
lukanisicc3 <103511037+lukanisicc3@users.noreply.github.com>
2023-09-12 19:04:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ else
     echo "export PATH=$PATH:/tmp/usr/bin/" >> /etc/shinit
 fi
 
-if grep -xqFe "export PATH=$PATH:/tmp/usr/bin/" /etc/shit; then
+if grep -xqFe "echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/usr/lib/" >> /etc/shinit" /etc/shit; then
     echo -e "Exists"
 else
     echo -e "Not Exists"
-    echo "export PATH=$PATH:/tmp/usr/bin/" >> /etc/shinit
+    echo "echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/usr/lib/" >> /etc/shinit" >> /etc/shinit
 fi
