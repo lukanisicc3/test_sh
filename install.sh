f786d95235260800afe5a1f2a3a643c5c15ff54f
lukanisicc3 <103511037+lukanisicc3@users.noreply.github.com>
2023-09-12 19:01:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,17 @@ opkg update
 opkg install python3 -d ram
 
 opkg install openssh-sftp-server
+
+if grep -xqFe "export PATH=$PATH:/tmp/usr/bin/" /etc/shit; then
+    echo -e "Exists"
+else
+    echo -e "Not Exists"
+    echo "export PATH=$PATH:/tmp/usr/bin/" >> /etc/shinit
+fi
+
+if grep -xqFe "export PATH=$PATH:/tmp/usr/bin/" /etc/shit; then
+    echo -e "Exists"
+else
+    echo -e "Not Exists"
+    echo "export PATH=$PATH:/tmp/usr/bin/" >> /etc/shinit
+fi
